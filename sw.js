25ad5c5d7c10b2e0ecb42424931e9fd1ec357d5f
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2026-02-23 20:03:13 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+/* offline-minimi */
+const CACHE_NAME = "crossfire-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon192.png",
+  "./pirates_crossfire_pulmasetti.json",
+  "./navy_ship.svg",
+  "./navy_ship_sinking.svg",
+  "./pirate_ship.svg",
+  "./pirate_ship_sinking.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(()=>cached))
+  );
+});
